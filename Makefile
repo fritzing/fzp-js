3b68a012d6bd214b9bbfffd1fa4ebc50f369b429
Paul Vollmer <paul.vollmer@protonmail.com>
2018-03-08 02:19:54 +0100

    Create tests script to parse all fzp files from the fritzing-parts repo

--- Makefile
+++ Makefile
@@ -3,5 +3,5 @@ include node_modules/fritzing-js/setup.mk
 test-load-data:
 	git clone git@github.com:fritzing/fritzing-parts.git test/fixtures/fritzing-parts
 test-load:
-	node test/partstest.js
+	node test/_fritzing-parts-test.js
 .PHONY: test-load-data test-load
---
+++ test/_fritzing-parts-test.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const {parseFZP} = require('../src/utils');
+
+let fzpDir = fs.readdirSync(path.join(__dirname, './fixtures/fritzing-parts/core'));
+
+// for (var i = 0; i < fzpDir.length; i++) {
+//   if (path.extname(fzpDir[i]) === ".fzp") {
+//
+//     const data = fs.readFileSync('./test/fixtures/LED-generic-3mm.fzp');
+//     parseFZP(data)
+//     .then((fzp) => {
+//       console.log('==> OK   ', fzpDir[i]);
+//     })
+//     .catch(e => {
+//       console.error('==> ERROR',fzpDir[i])
+//       console.error(e);
+//     })
+//     console.log(path.extname(fzpDir[i]));
+//   }
+// }
+
+eachPromise(fzpDir, (name, next) => {
+  if (path.extname(name) === '.fzp') {
+    const data = fs.readFileSync(path.join(__dirname, './fixtures/fritzing-parts/core', name));
+    parseFZP(data)
+    .then((fzp) => {
+      // console.log('==> OK   ', name);
+      next();
+    })
+    .catch((e) => {
+      console.error('\n==> ERROR', name);
+      console.error(e);
+      next();
+    });
+  }
+}, () => {
+  console.log('==> done');
+});
+
+function eachPromise(items, task, cb) {
+  let idx = 0;
+  let len = items.length;
+  iter();
+  function iter() {
+    task(items[idx++], function() {
+ idx >= len ? cb() : iter();
+});
+  }
+};
