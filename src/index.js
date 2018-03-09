b99a23826a6c4255a1e47547b56c3233a4eb3e0e
Paul Vollmer <paul.vollmer@protonmail.com>
2018-03-09 08:09:30 +0100

    Update path to connector-view at index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const FZP = require('./fzp/fzp');
 const FZPBus = require('./fzp/bus');
-const connector = require('./fzp/connector');
+const FZPConnector = require('./fzp/connector');
+const FZPConnectorView = require('./fzp/connector-view');
+const FZPProperty = require('./fzp/property');
 const FZPView = require('./fzp/view');
 const FZPUtils = require('./utils');
 
 module.exports = {
-  FZP: FZP,
-  FZPBus: FZPBus,
-  FZPConnector: connector.FZPConnector,
-  FZPConnectorView: connector.FZPConnectorView,
-  FZPView: FZPView,
-  FZPUtils: FZPUtils,
+  FZP,
+  FZPBus,
+  FZPConnector,
+  FZPConnectorView,
+  FZPProperty,
+  FZPView,
+  FZPUtils,
 };
